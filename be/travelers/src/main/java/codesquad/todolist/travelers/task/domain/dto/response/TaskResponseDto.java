8b1075c5d55bda8d05ff20829204abbf006a0e9f
CDBchan <kdjin1215@naver.com>
2023-07-19 16:39:52 +0900

    [BE] style: code style 포맷

--- be/travelers/src/main/java/codesquad/todolist/travelers/task/domain/dto/response/TaskResponseDto.java
+++ be/travelers/src/main/java/codesquad/todolist/travelers/task/domain/dto/response/TaskResponseDto.java
@@ -1,7 +1,6 @@
 package codesquad.todolist.travelers.task.domain.dto.response;
 
 import codesquad.todolist.travelers.task.domain.entity.Task;
-import java.time.LocalDateTime;
 
 public class TaskResponseDto {
     private final Long taskId;
